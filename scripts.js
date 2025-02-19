4d8005f2d63da7d78bafea74d1168aecbeaabfb2
lucas951 <lucas.augustosilva19@gmail.com>
2025-02-18 21:37:22 -0300

    adição de comentário

--- scripts.js
+++ scripts.js
@@ -2,10 +2,10 @@ const botaoSortear = document.querySelector("button")
 
 function sortear(){ 
 
-    const inputMin = Math.ceil(document.querySelector(".input-Min").value)
-    const inputMax = Math.floor(document.querySelector(".input-Max").value)
+    const inputMin = Math.ceil(document.querySelector(".input-Min").value) // ceil arrendondar para cima
+    const inputMax = Math.floor(document.querySelector(".input-Max").value) // floor arredondar para baixo
 
-    const result = Math.floor(Math.random() * (inputMax - inputMin + 1)) + inputMin
+    const result = Math.floor(Math.random() * (inputMax - inputMin + 1)) + inputMin 
 
     alert(result)
 }
